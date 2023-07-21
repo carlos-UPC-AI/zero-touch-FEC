6b36df6883e55e02655c6ce67ebfc215fb278815
Carlos R.M <117828418+carlos-UPC-AI@users.noreply.github.com>
2023-07-21 20:05:32 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,23 +6,24 @@ from environment import Environment
 from torch.optim import Adam
 from torch.nn import ReLU
 from config import (
-    ENV_SMALL, BACKGROUND_VEHICLES, SAVE_FREQ,
+    ENV_SMALL, ENV_MEDIUM, ENV_LARGE,BACKGROUND_VEHICLES, SAVE_FREQ,
     EVAL_EPISODES, PROJECT_DIR, LOGS, TRAINING_ROUNDS,
     CHECKPOINTS, MODELS, TIME_STEPS
 )
 
 # GLOBALS
+ENV = ENV_SMALL # select from: ENV_SMALL, ENV_MEDIUM or ENV_LARGE
 # Select from the three scenarios (small, medium, large)
-ENVIRONMENT = Environment(scenario=ENV_SMALL['scenario'],
-                          edges_cost=ENV_SMALL['edges_cost'],
-                          network=ENV_SMALL['network'],
-                          fec_coverage=ENV_SMALL['fec_coverage'],
-                          state_features=ENV_SMALL['state_features'],
-                          no_up_nodes=ENV_SMALL['no_up_nodes'],
-                          no_down_nodes=ENV_SMALL['no_down_nodes'],
-                          no_left_nodes=ENV_SMALL['no_left_nodes'],
-                          no_right_nodes=ENV_SMALL['no_right_nodes'],
-                          nodes=ENV_SMALL['nodes'],
+ENVIRONMENT = Environment(scenario=ENV['scenario'],
+                          edges_cost=ENV['edges_cost'],
+                          network=ENV['network'],
+                          fec_coverage=ENV['fec_coverage'],
+                          state_features=ENV['state_features'],
+                          no_up_nodes=ENV['no_up_nodes'],
+                          no_down_nodes=ENV['no_down_nodes'],
+                          no_left_nodes=ENV['no_left_nodes'],
+                          no_right_nodes=ENV['no_right_nodes'],
+                          nodes=ENV['nodes'],
                           route='random',  # Select from random: [[1, 16], [9, 12], [4, 13]], any_route or specific
                           # route: [source, target]
                           vehicles_num=BACKGROUND_VEHICLES)
